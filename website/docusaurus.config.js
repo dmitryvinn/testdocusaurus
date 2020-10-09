e6f142e0ca13b1116d2c8f28cf9ae6db75faa73c
Tejun Heo <tj@kernel.org>
2020-10-09 11:46:30 -0400

    xxx

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -10,8 +10,7 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://htejun.github.io/testdocusaurus/website/',
-  baseUrl: '/testdocusaurus/website/',
+  url: 'https://htejun.github.io/testdocusaurus',
   baseUrl: '/website/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
